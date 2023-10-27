d138300837cbfd7db5b016c9fb4009839534d48a
JubalThang <jubalthang@gmail.com>
2023-10-27 12:28:03 -0500

    trying to fix assets:precompile deploy error attemp 1

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ require "action_view/railtie"
 # require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
+# this to fix assets:precompile error
+require "sprockets/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
